34debd462713b53395cbbdab4d9d5e20d70aea14
Ln3242 <109390328+Ln3242@users.noreply.github.com>
2022-10-23 11:46:54 -0500

    Update ArrayList.lua

--- ArrayList.lua
+++ ArrayList.lua
@@ -1,4 +1,4 @@
-local array = Instance.new("ScreenGui", game.CoreGui)
+local array = Instance.new("ScreenGui", game:GetService("CoreGui"))
 local arrayFrame = Instance.new("Frame", array)
 arrayFrame.Size = UDim2.new(0.13, 0, 1, 0)
 arrayFrame.Position = UDim2.new(0.87, 0, 0, 0)
@@ -10,10 +10,9 @@ Grid.CellSize = UDim2.new(1, 0, 0.0275, 0)
 Grid.HorizontalAlignment = "Left"
 
 task.spawn(function()
-    repeat
-         task.wait(1)
-         array.Enabled = getgenv().Array
-     until true == false
+    while task.wait(1) do
+        array.Enabled = shared["CometConfigs"].Enabled
+     end
 end)
 
 Arraylist = {
@@ -21,7 +20,7 @@ Arraylist = {
         local Text = Instance.new("TextLabel",arrayFrame)
         local newName
         if Suffix then
-            newName = Name.." - "..Suffix
+			newName = Name.." | "..Suffix
         else
             newName = Name
         end
@@ -38,8 +37,8 @@ Arraylist = {
         task.spawn(function()
             repeat
                 task.wait()
-                Text.TextStrokeTransparency = getgenv().ArraylistStrokeTransparency
-                Text.TextColor3 = getgenv().HUDColor
+                Text.TextStrokeTransparency = shared["CometConfigs"].StrokeTransparency
+                Text.TextColor3 = rainbow()
             until not Text
         end)
     end,
@@ -49,15 +48,6 @@ Arraylist = {
         end
     end,
 }
-
-task.spawn(function()
-    task.wait(4)
-    repeat task.wait(1) until Uninjected == true
-    for i,v in pairs(array:GetChildren()) do
-        v:Destroy()
-    end
-    array:Destroy()
-end)
 
 
 return Arraylist
