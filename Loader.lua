c27442eb01aae05769f3d21edaa8dc3ee5bf9b66
Ln3242 <109390328+Ln3242@users.noreply.github.com>
2022-10-24 21:10:41 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,11 @@
 repeat task.wait() until game:IsLoaded()
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Ln3242/CometV2/main/Loader.lua'))()")
+    end
+end)
+
 function betterisfile(path)
     local suc, res = pcall(function() return readfile(path) end)
     return suc and res ~= nil
